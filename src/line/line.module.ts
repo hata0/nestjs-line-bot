885d0662b0aa94944359594feb7942a1773c8fa1
hata0 <haataa0100@gmail.com>
2024-08-15 15:24:05 +0900

    feat: カウントアップ処理を実装

--- src/line/line.module.ts
+++ src/line/line.module.ts
@@ -3,10 +3,11 @@ import { LineController } from './line.controller';
 import { LineService } from './line.service';
 import { ConfigService } from './config/config.service';
 import { RichMenuModule } from './rich-menu/rich-menu.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [LineController],
-  providers: [LineService, ConfigService],
+  providers: [LineService, ConfigService, PrismaService],
   imports: [RichMenuModule],
 })
 export class LineModule {}
--- src/line/line.service.ts
+++ src/line/line.service.ts
@@ -1,28 +1,57 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from './config/config.service';
 import { WebhookRequestBody } from '@line/bot-sdk';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class LineService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async handleWebhook(req: WebhookRequestBody): Promise<void> {
     const client = this.configService.createLinebotClient();
     const events = req.events;
 
-    events.forEach((event) => {
-      if (event.type === 'message') {
-        const returnMessage =
-          event.message.type === 'text' ? event.message.text : 'No Text';
-        client.replyMessage({
-          replyToken: event.replyToken,
-          messages: [
-            {
-              type: 'text',
-              text: returnMessage,
+    events.forEach(async (event) => {
+      if (event.type === 'message' && event.message.type === 'text') {
+        if (event.message.text === 'count') {
+          const user = await this.prisma.user.upsert({
+            where: {
+              id: event.source.userId,
             },
-          ],
-        });
+            update: {
+              count: {
+                increment: 1,
+              },
+            },
+            create: {
+              id: event.source.userId,
+              count: 1,
+            },
+          });
+
+          client.replyMessage({
+            replyToken: event.replyToken,
+            messages: [
+              {
+                type: 'text',
+                text: user.count.toString(),
+              },
+            ],
+          });
+        } else {
+          client.replyMessage({
+            replyToken: event.replyToken,
+            messages: [
+              {
+                type: 'text',
+                text: event.message.text,
+              },
+            ],
+          });
+        }
       }
     });
   }
