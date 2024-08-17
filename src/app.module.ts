2acae0244adb86a9283de3367151ee29c30e6b48
hata0 <haataa0100@gmail.com>
2024-08-17 22:10:08 +0900

    feat: カウントアップする api を実装

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { CountUpController } from './count-up/count-up.controller';
 import { AuthModule } from './auth/auth.module';
+import { ConfigService } from './line/config/config.service';
 
 @Module({
   imports: [
@@ -16,6 +17,6 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
   ],
   controllers: [AppController, CountUpController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, ConfigService],
 })
 export class AppModule {}
--- src/count-up/count-up.controller.ts
+++ src/count-up/count-up.controller.ts
@@ -1,11 +1,43 @@
-import { Controller, Put, UseGuards } from '@nestjs/common';
+import { VerifyIDToken } from '@line/bot-sdk';
+import { Controller, Put, Req, UseGuards } from '@nestjs/common';
 import { LineAuthGuard } from 'src/auth/line-auth.guard';
+import { ConfigService } from 'src/line/config/config.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('count-up')
 @UseGuards(LineAuthGuard)
 export class CountUpController {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly config: ConfigService,
+  ) {}
+
   @Put('')
-  putCountUp() {
-    return 'hello auth';
+  async putCountUp(@Req() req) {
+    const data: VerifyIDToken = req.user;
+    const user = await this.prisma.user.upsert({
+      where: {
+        id: data.sub,
+      },
+      update: {
+        count: {
+          increment: 1,
+        },
+      },
+      create: {
+        id: data.sub,
+        count: 1,
+      },
+    });
+    const client = this.config.createLinebotClient();
+    return client.pushMessage({
+      to: data.sub,
+      messages: [
+        {
+          type: 'text',
+          text: user.count.toString(),
+        },
+      ],
+    });
   }
 }
